233072c118892054ffcacfa67bd42b83087167e7
Jamil Dhanani <jamil.dhanani@gmail.com>
2018-04-22 16:43:02 -0700

    Fix layer numbers and print output shapes for each

--- main.py
+++ main.py
@@ -62,21 +62,12 @@ case = reader.get_case(ids[0])
 case_data = np.expand_dims(case['data'], 0)
 case_labels = np.expand_dims(case['labels'], 0)
 
-
-
-height, width, depth = reader.get_case(ids[0])['labels'].shape
+data, labels = preprocess(case_data, case_labels)
+batch_size, height, width, depth = data.shape
 print('Shape is ', (height, width, depth))
-
 
 input = tf.placeholder(dtype=tf.float16, shape=[None, height, width, 1])
 input = tf.stack([input, input, input], axis=3)
-
-
-
-
-
-
-
 
 resnet = tf.keras.applications.ResNet50(
     include_top=False,
@@ -86,15 +77,21 @@ resnet = tf.keras.applications.ResNet50(
     pooling=None,
 )
 #
-resnet_output5 = resnet.graph.get_tensor_by_name("activation_49/Relu:0") # (b, 7, 7, 2048)
-resnet_output4 = resnet.graph.get_tensor_by_name("activation_46/Relu:0") # (b, 14, 14, f)
-resnet_output3 = resnet.graph.get_tensor_by_name("activation_22/Relu:0") # (b, 28, 28, f)
-resnet_output2 = resnet.graph.get_tensor_by_name("activation_10/Relu:0") # (b, 56, 56, f)
+
+resnet_output5 = resnet.graph.get_tensor_by_name("activation_48/Relu:0") # (b, 7, 7, 2048)
+resnet_output4 = resnet.graph.get_tensor_by_name("activation_39/Relu:0") # (b, 14, 14, f)
+resnet_output3 = resnet.graph.get_tensor_by_name("activation_21/Relu:0") # (b, 28, 28, f)
+resnet_output2 = resnet.graph.get_tensor_by_name("activation_9/Relu:0") # (b, 56, 56, f)
 resnet_output1 = resnet.graph.get_tensor_by_name("activation/Relu:0")    # (b, 112, 112, f)
 resnet_output0 = input                                                   # (b, 224, 224, 3)
 
-
 print(resnet_output0.shape)
+print(resnet_output1.shape)
+print(resnet_output2.shape)
+print(resnet_output3.shape)
+print(resnet_output4.shape)
+print(resnet_output5.shape)
+
 
 with tf.Session() as sess:
     tf.summary.FileWriter(config.output_path, sess.graph)
