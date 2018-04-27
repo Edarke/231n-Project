93a99e0087e78bd289b880d91a7d7f4a14add0ad
Jamil Dhanani <jamil.dhanani@gmail.com>
2018-04-27 11:19:12 -0700

    New soft dice score function

--- metrics.py
+++ metrics.py
@@ -1,14 +1,21 @@
 import tensorflow as tf
 
 
-def soft_dice(predictions, labels):
-    # 2TP / (T - TP + 2TP + P - TP)
-    predictions = tf.reshape(predictions, [-1])
-    truth = tf.reshape(labels, [-1])
-    intersection = 2*tf.reduce_sum(predictions * truth)
-    union = tf.reduce_sum(predictions + truth)
-    union = tf.Print(union, [intersection, union])
-    return 1 - intersection/(union + 1e-7)
+# def soft_dice(predictions, labels):
+#     # 2TP / (T - TP + 2TP + P - TP)
+#     predictions = tf.reshape(predictions, [-1])
+#     truth = tf.reshape(labels, [-1])
+#     intersection = 2*tf.reduce_sum(predictions * truth)
+#     union = tf.reduce_sum(predictions + truth)
+#     union = tf.Print(union, [intersection, union])
+#     return 1 - intersection/(union + 1e-7)
+
+def soft_dice(y_pred, y_true):
+    smooth = 1e-7
+    y_true_f = tf.reshape(y_true, [-1])
+    y_pred_f = tf.reshape(y_pred, [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (tf.reduce_sum(y_true_f*y_true_f) + tf.reduce_sum(y_pred_f*y_pred_f) + smooth)
 
 
 def soft_iou(predictions, labels):
