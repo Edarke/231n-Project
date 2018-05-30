b9aeafbb9c2a61b4a9afdb817fd0495f7032d99a
Jamil Dhanani <jamil.dhanani@gmail.com>
2018-05-30 19:29:23 +0000

    Add visualization back

--- predict_callback.py
+++ predict_callback.py
@@ -15,16 +15,16 @@ class PredictCallback(keras.callbacks.Callback):
         self.generator = reader
 
     def on_epoch_end(self, epoch, logs=None):
-#       originals, processed, targets = self.generator.get_sample_cases()
-#       predictions = self.model.predict(processed)
+        originals, processed, targets = self.generator.get_sample_cases()
+        predictions = self.model.predict(processed)
 
         output_dir = self.path + str(epoch) + '/'
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
-#       for i, original in enumerate(originals):
-#           ex = eval.visualize(original, predictions[i], targets[i])
-#           ex.save(output_dir + str(i) + '.jpg', 'JPEG')
+        for i, original in enumerate(originals):
+            ex = eval.visualize(original, predictions[i], targets[i])
+            ex.save(output_dir + str(i) + '.jpg', 'JPEG')
 
 #           crf_predictions = process_crf(processed[i, :, :, :], predictions[i, :, :, :]) # (224, 224, 4)
 #           crf_ex = eval.visualize(original, crf_predictions, targets[i])
